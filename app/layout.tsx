99836b753f659c2643e8f6e19eb27206053c58f8
sajinits64-eng <sajinits64@gmail.com>
2025-09-30 16:40:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,55 @@
+import React from "react";
+import "../styles/main.css"; // Adjust path to your CSS location
+
+const ElectricalAnalysisSection: React.FC = () => {
+  return (
+    <div className="v2_3957">
+      <div className="v2_3958">
+        <div className="v2_3959"></div>
+        <div className="v2_3960">
+          <div className="name"></div>
+          <div className="name"></div>
+          <div className="name"></div>
+          <div className="name"></div>
+        </div>
+        <div className="v2_3965">
+          <div className="name"></div>
+        </div>
+        <div className="v2_3967">
+          <div className="v2_3968">
+            <div className="v2_3969"></div>
+          </div>
+          <div className="v2_3970">
+            <div className="v2_3971"></div>
+          </div>
+        </div>
+      </div>
+      <div className="v2_3972">
+        <div className="v2_3973"></div>
+        <span className="v2_3974">Proactive Safety: Reducing Arc Flash Risks in Australia</span>
+        <span className="v2_3975">
+          Australia has some of the most stringent frameworks in electrical safety, like AS/NZS 3000( For Wiring), AS/NZS 4836 ( For Safe Working Practices), etc....
+        </span>
+        <div className="v2_3976">
+          <span className="v2_3977">Category</span>
+          <span className="v2_3978">Author</span>
+          <span className="v2_3979">●</span>
+          <span className="v2_3980">●</span>
+          <span className="v2_3981">5 min read</span>
+        </div>
+        <div className="v2_3982">
+          <span className="v2_3983">Read more</span>
+          <div className="v2_3984">
+            <div className="v2_3985"></div>
+          </div>
+        </div>
+      </div>
+      
+      {/* Continue the rest of your HTML content conversion similarly */}
+      
+    </div>
+  );
+};
+
+export default ElectricalAnalysisSection;
 
